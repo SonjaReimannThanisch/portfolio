852d68af944438d1a0a439a09b7b5aab6dedeaeb
SonjaReimannThanisch <137377644+SonjaReimannThanisch@users.noreply.github.com>
2026-06-01 18:26:54 +0200

    clean up

--- scripts/render.js
+++ scripts/render.js
@@ -52,7 +52,7 @@ function renderMySkills() {
             <img src="./assets/icons/Property 1=Challenge me.png">
         </div>
         <div class="skills-side-text">
-            <p class="skills-text">
+            <p class="skills-side-text">
                 Mich motiviert die Möglichkeit, jeden Tag etwas Neues zu lernen. Ob neue Technologien, bessere Lösungswege oder ungewohnte Herausforderungen: Ich habe Freude daran, mich kontinuierlich weiterzuentwickeln und mein Wissen durch praktische Projekte auszubauen.
             </p>
         </div>
