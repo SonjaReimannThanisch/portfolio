ccfa3c87e335793890ebba45df25fe68d3b88bd4
SonjaReimannThanisch <137377644+SonjaReimannThanisch@users.noreply.github.com>
2026-07-30 15:08:34 +0200

    Update render.js

--- scripts/render.js
+++ scripts/render.js
@@ -1,14 +1,13 @@
 function renderWhyMe() {
+    let text = translations[currentLanguage].about;
     let aboutSection = document.getElementById("about");
     aboutSection.innerHTML = `
-        <h2 class="why-me-title">Why me</h2>
+        <h2 class="why-me-title">${text.title}</h2>
         <div id="whyMe" class="why-me-content">
             <p class="why-me-text">
-                Was mich an der Softwareentwicklung begeistert, ist die Möglichkeit, komplexe Probleme in kleine, lösbare Schritte zu zerlegen. Ich habe Freude daran, neue Technologien zu verstehen, Fehler zu analysieren und so lange an einer Lösung zu arbeiten, bis sie funktioniert.
-                Als Quereinsteigerin habe ich gelernt, dass Fortschritt nicht immer geradlinig verläuft. Viele Herausforderungen haben mich Geduld, Ausdauer und strukturiertes Arbeiten gelehrt. Gerade diese Erfahrungen haben meinen Wunsch gestärkt, mich kontinuierlich weiterzuentwickeln und mein Wissen auszubauen.
-                Für mich bedeutet Programmieren, jeden Tag etwas Neues zu lernen und Ideen in funktionierende Anwendungen zu verwandeln. Genau diese Kombination aus Kreativität, Logik und lebenslangem Lernen macht diesen Bereich für mich so spannend.
+                ${text.text}
             </p>
-            <h3>I am</h3>
+            <h3>${text.subtitle}</h3>
             <div class="why-me-facts">
                 <div class="fact-card first">
                     <img class="icon-default" src="./assets/icons/location.png" alt="Location">
@@ -23,7 +22,7 @@ function renderWhyMe() {
                     <img class="icon-hover" src="./assets/icons/Property 1=hover (2).png" alt="Relocation hover">
                 </div>
             </div>
-            <a class="contact-button" href="#contact">Contact me</a>
+            <a class="contact-button" href="#contact">${text.contactButton}</a>
             <a class="why-me-next-link" href="#skills"><img src="./assets/icons/Property 1=Default.png" alt="Next Page"></a>
         </div>
     `;
